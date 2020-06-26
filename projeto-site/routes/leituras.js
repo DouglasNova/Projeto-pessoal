9a0e88fd6ea69d15b32573d27386bf96cd63a119
unknown <douglas.nova@bandtec.com.br>
2020-06-26 08:57:42 -0300

    atualização

---
+++ projeto-site/routes/leituras.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+var sequelize = require('../models').sequelize;
+var Leitura = require('../models').Leitura;
+
+/* Recuperar as últimas N leituras */
+router.get('/ultimas', function (req, res, next) {
+
+	// quantas são as últimas leituras que quer? 8 está bom?
+	const limite_linhas = 7;
+
+	console.log(`Recuperando as últimas ${limite_linhas} leituras`);
+
+	const instrucaoSql = `select top ${limite_linhas} 
+						nome, 
+						twitter
+						from cadastrar `;
+
+	sequelize.query(instrucaoSql, {
+		model: Leitura,
+		mapToModel: true
+	})
+		.then(resultado => {
+			console.log(`Encontrados: ${resultado.length}`);
+			res.json(resultado);
+		}).catch(erro => {
+			console.error(erro);
+			res.status(500).send(erro.message);
+		});
+});
+
+
+
+
+module.exports = router;
